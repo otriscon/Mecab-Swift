6c541677c4e8a9db2c89218e80b2f4d107087f37
Oscar Triscon <github@triscon.com>
2026-02-24 21:06:41 -0500

    Fix vendoring: populate mecab/src/ via --recurse-submodules

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Mecab-Swift",
     defaultLocalization: "en",
-    platforms: [.macOS(.v10_11), .iOS(.v16)],
+    platforms: [.macOS(.v13), .iOS(.v16)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -112,7 +112,6 @@ let package = Package(
                 cSettings: [.define("HAVE_CONFIG_H"),
                         .headerSearchPath(".")],
                 cxxSettings: [.define("HAVE_ICONV")],
-                swiftSettings: nil,
                 linkerSettings: [.linkedLibrary("iconv")]),
     ]
 )
